8f1319b9e39b8ca2aa9a101689ac5101642e3358
Zuzanna <zbieszczad@interia.pl>
2026-02-16 22:37:01 +0100

    Add Vector3d class with basic vector operations

--- core/Particle.h
+++ core/Particle.h
@@ -1,2 +1,4 @@
 #pragma once
+#include <cmath>
 #include "../math/Vec3.h"
+
--- math/Vec3.h
+++ math/Vec3.h
@@ -1,1 +1,31 @@
 #pragma once
+
+class Vector3d{
+public:
+    
+    Vector3d();
+    Vector3d(double x, double y, double z);
+
+    Vector3d add(const Vector3d &other) const;
+    Vector3d subtract(const Vector3d &other) const;
+    Vector3d scale(double value) const;
+    Vector3d cross(const Vector3d &other) const;
+    double dot(const Vector3d &other) const;
+    double magnitude() const;
+
+    void addInPlace(const Vector3d &other);
+    void subtractInPlace(const Vector3d &other);
+    void scaleInPlace(double value);
+    void crossInPlace(double value);
+
+    
+    double getX() const;
+    double getY() const;
+    double getZ() const;
+
+private:
+    double x=0;
+    double y=0;
+    double z=0;
+    
+};
