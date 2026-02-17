e1f814f3ef4196be959a846ce3a9f3a325be601f
Zuzanna <zbieszczad@interia.pl>
2026-02-17 16:56:39 +0100

    Implemented Particle class with constructors, getters/setters, and force handling

--- core/Particle.cpp
+++ core/Particle.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
-#include "Vec3.h"
+#include "../math/Vec3.h"
 #include <cmath>
 #include <ostream>
+#include "Particle.h"
+
+
+// getters 
+const Vector3d& Particle:: getPosition() const{
+    return _position;
+}
+
+const Vector3d& Particle:: getVelocity() const{
+    return _velocity;
+}
+
+double Particle:: getMass() const{
+    return _mass;
+}
+
+
+double Particle:: getRadius() const{
+    return _radius;
+}
+
+Vector3d Particle:: getForce() const{
+    return _force;
+}
+
+// setters
+void Particle::setPosition(const Vector3d& other){
+    _position=other;
+}
+
+void Particle::setVelocity(const Vector3d& other){
+    _velocity=other;
+}
+
+void Particle::setAcceleration(const Vector3d& other){
+    _acceleration=other;
+}
+
+
+void Particle:: applyForce(const Vector3d& force){
+    _force=_force+force;
+}
+
+void Particle:: setColor(const Vector3d& color){
+    _color=color;
+}
+
+void Particle::resetForces(){
+    _force = Vector3d(0,0,0);
+}
 
 
--- core/Particle.h
+++ core/Particle.h
@@ -5,7 +5,8 @@
 
 class Particle{
 public:
-    Particle::Particle(const Vector3d& position,
+    //constructors
+    Particle(const Vector3d& position,
                    const Vector3d& velocity,
                    const Vector3d& acceleration,
                    const Vector3d& spin,
@@ -27,23 +28,50 @@ public:
       _type(type)
     {}
 
+    Particle()
+    : _position(0,0,0),
+      _velocity(0,0,0),
+      _acceleration(0,0,0),
+      _spin(0,0,0),
+      _color(1,1,1),     
+      _force(0,0,0),
+      _mass(1.0),
+      _radius(1.0),
+      _degreesOfFreedom(3),
+      _type("default")
+    {}
 
-    Particle();
+    
     Particle(const Vector3d& position,
          const Vector3d& velocity,
          double mass,
-         double radius);
+         double radius)
+    : _position(position),
+      _velocity(velocity),
+      _acceleration(0,0,0),
+      _spin(0,0,0),
+      _color(1,1,1),
+      _force(0,0,0),
+      _mass(mass),
+      _radius(radius),
+      _degreesOfFreedom(3),
+      _type("default")
+    {}
 
+
+    // methods
     const Vector3d& getPosition() const;
     const Vector3d& getVelocity() const;
     double getMass() const;
     double getRadius() const;
+    Vector3d getForce() const;
 
-    void setPosition(const Vector3d&);
-    void setVelocity(const Vector3d&);
-    void setAcceleration(const Vector3d&);
+    void setPosition(const Vector3d& other);
+    void setVelocity(const Vector3d& other);
+    void setAcceleration(const Vector3d& other);
     void applyForce(const Vector3d& force);
     void setColor(const Vector3d& color);
+    void resetForces();
 
        
 private:
